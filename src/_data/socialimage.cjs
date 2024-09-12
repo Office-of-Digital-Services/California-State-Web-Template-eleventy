f39d43f4a9fe81bf0b7ae6c27e0b5e9cad0e60e1
Carter Medlin <carterm@users.noreply.github.com>
2024-09-12 09:28:12 -0700

    socialimage

---
+++ src/_data/socialimage.cjs
@@ -0,0 +1,10 @@
+//@ts-check
+
+// Default social media image
+// Can be overridden in page template front matter
+//
+// eg.
+// socialimage: https://mydomain.ca.gov/myimage.png
+
+module.exports =
+  "https://cdn.cdt.ca.gov/cdt/statetemplate/global/images/social_media_preview_image.png";
