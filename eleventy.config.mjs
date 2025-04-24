aeb8fedf9b031ffc0a73e252382bc5facf5b57ff
Carter Medlin <carterm@users.noreply.github.com>
2025-04-24 11:46:42 -0700

    module

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,15 +1,12 @@
 //@ts-check
-
 const domain = "https://template.webstandards.ca.gov";
 
 /**
- * Configures Eleventy with the specified settings.
- * @param {import("@11ty/eleventy").UserConfig} eleventyConfig - The Eleventy configuration object.
- * @returns {Promise<import("@11ty/eleventy").UserConfig>} The configured Eleventy object.
+ *
+ * @param {import("@11ty/eleventy").UserConfig} eleventyConfig
+ * @returns
  */
-module.exports = async function (eleventyConfig) {
-  const { EleventyRenderPlugin } = await import("@11ty/eleventy");
-
+export default async function (eleventyConfig) {
   // Copy `src/css/` to `_site/css`, `src/images/` to `_site/images`
   // Copy all static files that should appear in the website root
   // Copy state template code files from NPM
@@ -20,7 +17,6 @@ module.exports = async function (eleventyConfig) {
     "node_modules/@cagovweb/state-template/dist": "state-template"
   });
 
-  // Sorted list of all the samples
   eleventyConfig.addFilter(
     "canonical",
     (/** @type {{url:string}} */ page) => domain + page.url
@@ -28,20 +24,19 @@ module.exports = async function (eleventyConfig) {
 
   // allow nunjucks templating in .html files
   eleventyConfig.setTemplateFormats(["html", "njk", "11ty.js", "md"]);
-  const nunjucks = require("nunjucks");
-  const njkEnv = new nunjucks.Environment();
-  njkEnv.addFilter(
+
+  eleventyConfig.addFilter(
     "canonical",
     (/** @type {{url:string}} */ page) => domain + page.url
   );
-  eleventyConfig.addPlugin(EleventyRenderPlugin);
+
   eleventyConfig.addGlobalData("domain", domain);
 
   eleventyConfig.addWatchTarget("./src/css/");
   eleventyConfig.addWatchTarget("./src/images/");
 
-  eleventyConfig.setBrowserSyncConfig({
-    files: ["_site/**/*"]
+  eleventyConfig.setServerOptions({
+    watch: ["_site/**/*"]
   });
 
   eleventyConfig.addPassthroughCopy("src/_data");
@@ -62,4 +57,4 @@ module.exports = async function (eleventyConfig) {
     pathPrefix: "/",
     dataTemplateEngine: false
   };
-};
+}
